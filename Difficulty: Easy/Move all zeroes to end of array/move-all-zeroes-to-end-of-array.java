8c4d03fbf60b5f7478da8ef35108ba1fb136293b
Aryan Bhoi <159808093+AryanBhoi0005@users.noreply.github.com>
2024-10-16 22:47:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move all zeroes to end of array/move-all-zeroes-to-end-of-array.java
@@ -0,0 +1,49 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String[] inputLine = br.readLine().split(" ");
+            int[] arr = new int[inputLine.length];
+            for (int i = 0; i < inputLine.length; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().pushZerosToEnd(arr);
+            int n = arr.length;
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+class Solution {
+    void pushZerosToEnd(int[] arr) {
+        int n = arr.length;
+        int index = 0; // Pointer to track the position for the next non-zero element
+
+        // Move all non-zero elements to the front of the array
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        // Fill the remaining positions in the array with zeros
+        while (index < n) {
+            arr[index] = 0;
+            index++;
+        }
+    }
+}
