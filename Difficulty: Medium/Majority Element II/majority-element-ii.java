cf58e8f476f8072fd6320faaaf4cc2be1404ea3c
Aryan Bhoi <159808093+AryanBhoi0005@users.noreply.github.com>
2024-11-20 11:56:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Majority Element II/majority-element-ii.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.nextLine(); // Consume the newline character
+
+        while (t-- > 0) {
+            String s = sc.nextLine();
+            String[] parts = s.split(" ");
+            int[] nums = new int[parts.length];
+            for (int i = 0; i < parts.length; i++) {
+                nums[i] = Integer.parseInt(parts[i]);
+            }
+            Solution ob = new Solution();
+            List<Integer> ans = ob.findMajority(nums);
+
+            if (ans.size() == 0) {
+                System.out.println("[]");
+            } else {
+                for (int i : ans) {
+                    System.out.print(i + " ");
+                }
+                System.out.println();
+            }
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to find the majority elements in the array
+    public List<Integer> findMajority(int[] nums) {
+        // Your code goes here.
+        int n=nums.length/3;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        for(int key:map.keySet()){
+            if(map.get(key)>n){
+                list.add(key);
+            }
+        }
+        Collections.sort(list);
+        return list;
+    }
+}
