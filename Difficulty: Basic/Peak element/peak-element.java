4ec9fe669a11ef8425ed19d21babf9afa08086b2
Aryan Bhoi <159808093+AryanBhoi0005@users.noreply.github.com>
2024-12-15 11:02:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Peak element/peak-element.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            String arr[] = br.readLine().split(" ");
+            int a[] = new int[arr.length];
+
+            for (int i = 0; i < arr.length; i++) {
+                a[i] = Integer.parseInt(arr[i]);
+            }
+            Solution obj = new Solution();
+            int f = 0;
+            int idx = obj.peakElement(a);
+            int n = a.length;
+            if (idx < 0 && idx >= n)
+                System.out.println("false");
+            else {
+                if (n == 1 && idx == 0)
+                    f = 1;
+                else if (idx == 0 && a[0] > a[1])
+                    f = 1;
+                else if (idx == n - 1 && a[n - 1] > a[n - 2])
+                    f = 1;
+                else if (idx > 0 && idx < n && a[idx] > a[idx + 1] &&
+                         a[idx] > a[idx - 1])
+                    f = 1;
+                else
+                    f = 0;
+                if (f == 1) {
+                    System.out.println("true");
+                } else {
+                    System.out.println("false");
+                }
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    public int peakElement(int[] arr) {
+        // code here
+        if(arr.length==1){
+            return 0;
+        }
+        int s=0;
+        int e=arr.length-1;
+        while(s<e){
+        int m=(s+e)/2;
+          if(arr[m]<arr[m+1]){
+             s=m+1; 
+          }  
+          else{
+            e=m;
+          }
+        }
+        return s;
+    }
+}
